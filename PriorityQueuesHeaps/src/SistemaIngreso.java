ab80e604049b5d90b1391dc19113f84231c0ce3a
Pablo Sao <pablo_sao@outlook.com>
2019-03-27 11:40:00 -0600

    Se agregan clases a reutilizar de proyectos anteriores

---
+++ PriorityQueuesHeaps/src/SistemaIngreso.java
@@ -0,0 +1,12 @@
+/***
+ * Clase principal para el manejo de la aplicación
+ * @author Pablo Sao
+ * @version 27/03/2019
+ */
+
+public class SistemaIngreso {
+
+    public static void main(String[] args){
+
+    }
+}
